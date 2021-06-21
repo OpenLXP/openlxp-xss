f9f539b5573c200d595698d0e99075edd4549203
Jean Diouf <jdiouf@deloitte.com>
2021-06-21 10:44:28 -0400

    added test setup to reuse values

--- app/api/tests/test_views_unit.py
+++ app/api/tests/test_views_unit.py
@@ -4,13 +4,12 @@ from unittest.mock import patch
 from django.test import tag
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APITestCase
 
-from core.models import SchemaLedger, TransformationLedger
+from .test_setup import TestSetUp
 
 
 @tag('unit')
-class ViewTests(APITestCase):
+class ViewTests(TestSetUp):
 
     def test_schemaledger_requests_no_param(self):
         """Test that making a get request to the schema api with no query
@@ -29,22 +28,17 @@ class ViewTests(APITestCase):
         url = "%s?name=test" % (reverse('api:schemaledger'))
 
         with patch('api.views.SchemaLedger.objects') as schemaObj:
-            schemaLedger = SchemaLedger(schema_name="test_name",
-                                        schema_iri="schema_iri",
-                                        metadata="metadata",
-                                        status="published",
-                                        version="1.2.3")
             schemaObj.return_value = schemaObj
             schemaObj.all.return_value = schemaObj
             schemaObj.filter.return_value = schemaObj
             schemaObj.order_by.return_value = schemaObj
-            schemaObj.first.return_value = schemaLedger
+            schemaObj.first.return_value = self.schema
 
             response = self.client.get(url)
             responseDict = json.loads(response.content)
 
             self.assertEqual(response.status_code, status.HTTP_200_OK)
-            self.assertEqual(responseDict["schema_name"], "test_name")
+            self.assertEqual(responseDict["schema_name"], self.schema_name)
 
     def test_schemaledger_requests_all_params(self):
         """Test that making a get request to the schema api with just all
@@ -52,22 +46,17 @@ class ViewTests(APITestCase):
         url = "%s?name=test&version=1.0.0" % (reverse('api:schemaledger'))
 
         with patch('api.views.SchemaLedger.objects') as schemaObj:
-            schemaLedger = SchemaLedger(schema_name="test_name",
-                                        schema_iri="schema_iri",
-                                        metadata="metadata",
-                                        status="published",
-                                        version="1.2.3")
             schemaObj.return_value = schemaObj
             schemaObj.all.return_value = schemaObj
             schemaObj.filter.return_value = schemaObj
             schemaObj.order_by.return_value = schemaObj
-            schemaObj.first.return_value = schemaLedger
+            schemaObj.first.return_value = self.schema
 
             response = self.client.get(url)
             responseDict = json.loads(response.content)
 
             self.assertEqual(response.status_code, status.HTTP_200_OK)
-            self.assertEqual(responseDict["schema_name"], "test_name")
+            self.assertEqual(responseDict["schema_name"], self.schema_name)
 
     def test_schemaledger_requests_no_schema_found(self):
         """Test that making a get request to the schema api with just a schema
@@ -75,16 +64,11 @@ class ViewTests(APITestCase):
         url = "%s?name=test&version=1.0.2" % (reverse('api:schemaledger'))
 
         with patch('api.views.SchemaLedger.objects') as schemaObj:
-            schemaLedger = SchemaLedger(schema_name="test_name",
-                                        schema_iri="schema_iri",
-                                        metadata="metadata",
-                                        status="published",
-                                        version="1.2.3")
             schemaObj.return_value = schemaObj
             schemaObj.all.return_value = schemaObj
             schemaObj.filter.return_value = None
             schemaObj.order_by.return_value = schemaObj
-            schemaObj.first.return_value = schemaLedger
+            schemaObj.first.return_value = self.schema
 
             response = self.client.get(url)
             responseDict = json.loads(response.content)
@@ -125,20 +109,14 @@ class ViewTests(APITestCase):
                "targetVersion=1.0.0") % (reverse('api:transformationledger'))
 
         with patch('api.views.TransformationLedger.objects') as mappingObj:
-            transformationLedger = \
-                TransformationLedger(source_schema_name="sourceNm",
-                                     target_schema_name="targetNm",
-                                     source_schema_version="sourceVrs",
-                                     target_schema_version="targetVrs",
-                                     schema_mapping="mapping",
-                                     status="published")
             mappingObj.return_value = mappingObj
             mappingObj.all.return_value = mappingObj
             mappingObj.filter.return_value = mappingObj
-            mappingObj.first.return_value = transformationLedger
+            mappingObj.first.return_value = self.mapping
 
             response = self.client.get(url)
             responseDict = json.loads(response.content)
 
             self.assertEqual(response.status_code, status.HTTP_200_OK)
-            self.assertEqual(responseDict["source_schema_name"], "sourceNm")
+            self.assertEqual(responseDict["source_schema_name"],
+                             self.source_schema_name)
