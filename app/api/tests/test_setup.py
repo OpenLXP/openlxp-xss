b6775ef75257b0628bccfa9ee75440b6b26ecb10
JDTobin <90791836+JDTobin@users.noreply.github.com>
2022-04-21 11:03:49 -0400

    add coveragerc fix failing tests add more tests

--- app/api/tests/test_setup.py
+++ app/api/tests/test_setup.py
@@ -7,14 +7,39 @@ class TestSetUp(APITestCase):
 
     def setUp(self):
         """Function to set up necessary data for testing"""
+
+        self.metadata = {
+            "Course": {
+                "CourseProviderName": "Required",
+                "DepartmentName": "Optional",
+                "CourseCode": "Required",
+                "CourseTitle": "Required",
+                "CourseShortDescription": "Required",
+                "CourseAudience": "Required",
+                "CourseSectionDeliveryMode": "Optional",
+                "CourseObjective": "Optional",
+                "CoursePrerequisites": "Required",
+                "EstimatedCompletionTime": "Optional",
+                "CourseSpecialNotes": "Optional",
+                "CourseAdditionalInformation": "Optional",
+                "CourseURL": "Optional",
+                "CourseLevel": "Optional",
+                "CourseSubjectMatter": "Required",
+                "CourseCareerCategory": "Optional",
+                "CourseAdministratorInstruction": "Optional",
+                "CourseApplicationProcess": "Optional",
+                "CourseCredits": "Optional"
+            }
+            }
+
         self.sourceSchema = SchemaLedger(schema_name="test_name",
                                          schema_iri="test_name_1.2.3",
-                                         metadata="metadata",
+                                         metadata=self.metadata,
                                          status="published",
                                          version="1.2.3")
         self.targetSchema = SchemaLedger(schema_name="test_name_1",
                                          schema_iri="test_name_1_1.2.4",
-                                         metadata="metadata_1",
+                                         metadata=self.metadata,
                                          status="published",
                                          version="1.2.4")
         self.mapping = TransformationLedger(source_schema=self.sourceSchema,
@@ -23,7 +48,6 @@ class TestSetUp(APITestCase):
                                             status="published")
         self.schema_name = "test_name"
         self.schema_iri = "schema_iri"
-        self.metadata = "metadata"
         self.status = "published"
         self.version = "1.2.3"
         self.source_schema = "sourceNm"
