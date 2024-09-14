4f45b779802d18f5568a5dc8434f70d0b0b137fb
Simeranjeet Sandhu <simeransandhu@deloitte.com>
2024-09-14 12:56:53 -0700

    Modified start-app script

--- start-app.sh
+++ start-app.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 # start-server.sh
 
-cd /tmp/openlxp-xss/app
+/tmp/openlxp-xss/app
 python manage.py waitdb 
 python manage.py migrate 
 python manage.py loaddata admin_theme_data.json 
 if [ -n "$TMP_SCHEMA_DIR" ] ; then
-    (cd openlxp-xss; install -d -o 1001 -p $TMP_SCHEMA_DIR)
+    (cd /tmp/openlxp-xss/app; install -d -o 1001 -p $TMP_SCHEMA_DIR)
 else
-    (cd openlxp-xss; install -d -o 1001 -p tmp/schemas)
+    (cd /tmp/openlxp-xss/app; install -d -o 1001 -p tmp/schemas)
 fi
-cd /tmp/app/
+cd /tmp/
 pwd 
 # service clamav-daemon restart
 ./start-server.sh
